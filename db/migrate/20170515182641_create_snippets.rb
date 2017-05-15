ffbd007e49ab9ef11f2e3b3b7e69c8d336566492
vic6 <vicmatthews116@gmail.com>
2017-05-15 13:05:34 -0700

    Add rubocop rules, user model tests

--- db/migrate/20170515182641_create_snippets.rb
+++ db/migrate/20170515182641_create_snippets.rb
@@ -3,6 +3,7 @@ class CreateSnippets < ActiveRecord::Migration[5.1]
     create_table :snippets do |t|
       t.string :title
       t.text :content
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: 'vic6', email: 'chocolate@icecream.com',
+                     password: 'hello6', password_confirmation: 'hello6')
+  end
+
+  test 'user should be valid' do
+    assert @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '   '
+    assert_not @user.valid?
+  end
 end
