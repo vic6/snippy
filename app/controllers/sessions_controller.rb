281be7da525d6b8c97694306ea777db6392efe4d
vic6 <vicmatthews116@gmail.com>
2017-05-15 15:31:51 -0700

    Add sessions controller and home page, add sessions helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or(user)
+    else
+      # flash.now dissapears as soon as there is an additional request
+      flash.now[:danger] = 'Incorrect email/password'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+
+  root 'static_pages#home'
+  resources :users
 end
