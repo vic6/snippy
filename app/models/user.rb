39a624fb535db72b84402a7b7f76aa6f55095ed7
vic6 <vicmatthews116@gmail.com>
2017-05-15 13:41:42 -0700

    Add awesome print gem, fix typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :snippets
 
   before_save { email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     length: { maximum: 255 },
