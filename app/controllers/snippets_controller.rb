2c552cd1ddb410155f05ef96c857cc8a2681769f
vic6 <vicmatthews116@gmail.com>
2017-05-16 14:02:48 -0700

    Add snippets controller actions, fix routes typo

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,2 +1,19 @@
 class SnippetsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
 end
