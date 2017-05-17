d2c86389d0ad56b17aabc2a8f80e5c88cba499da
vic6 <vicmatthews116@gmail.com>
2017-05-16 19:10:55 -0700

    Add snippet create method

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -6,11 +6,24 @@ class SnippetsController < ApplicationController
   end
 
   def create
+    @snippet = current_user.snippets.build(snippet_params)
+    if @snippet.save
+      flash[:success] = 'Snippet created'
+      redirect_to current_user
+    else
+      render 'static_pages/home'
+    end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def snippet_params
+    params.require(:snippet).permit(:title, :content)
   end
 end
