af2a4d7cf3665c11e6208c3f6eaf531d19cf914b
vic6 <vicmatthews116@gmail.com>
2017-05-16 20:32:21 -0700

    Fix typo in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(name: "Lvl 80 Admin",
+User.create!(username: "Lvl 80 Admin",
              email: "vic@admin.com",
              password: "password",
              password_confirmation: "password",
